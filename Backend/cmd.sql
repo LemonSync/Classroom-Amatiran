c1b9cc013a524f42c8e2d49c19d4f74e1b121de6
lemonsync <grouplemon0@gmail.com>
2026-05-14 22:41:35 +0700

    sql structure update

--- Backend/cmd.sql
+++ Backend/cmd.sql
@@ -28,25 +28,28 @@ CREATE TABLE IF NOT EXISTS `akun` (
   PRIMARY KEY (`id`),
   KEY `id_siswa` (`id_siswa`),
   CONSTRAINT `akun_ibfk_1` FOREIGN KEY (`id_siswa`) REFERENCES `siswa` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- Dumping data for table db_classroom.akun: ~1 rows (approximately)
+-- Dumping data for table db_classroom.akun: ~2 rows (approximately)
 INSERT INTO `akun` (`id`, `id_siswa`, `email`, `password`) VALUES
 	(1, 1, 'simboloneres@gmail.com', 'Lemon098Cool!'),
-	(3, 2, 'grouplemon0@gmail.com', 'LemonKece');
+	(3, 2, 'grouplemon0@gmail.com', 'LemonKece'),
+	(4, 3, 'simboloneres@gmail.com', '123');
 
 -- Dumping structure for table db_classroom.siswa
 CREATE TABLE IF NOT EXISTS `siswa` (
   `id` int NOT NULL AUTO_INCREMENT,
   `nama` varchar(100) NOT NULL,
   `kelas` varchar(100) NOT NULL,
+  `login` tinyint DEFAULT '0',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- Dumping data for table db_classroom.siswa: ~1 rows (approximately)
-INSERT INTO `siswa` (`id`, `nama`, `kelas`) VALUES
-	(1, 'ERES FRAN SETIA SIMBOLON', 'XI-RPL-2'),
-	(2, 'Reymond Leonard', 'XI-RPL-2');
+-- Dumping data for table db_classroom.siswa: ~3 rows (approximately)
+INSERT INTO `siswa` (`id`, `nama`, `kelas`, `login`) VALUES
+	(1, 'ERES FRAN SETIA SIMBOLON', 'XI-RPL-2', 1),
+	(2, 'REYMOND LEONARD', 'XI-RPL-2', 0),
+	(3, 'JOLI IRAWAN ZAI', 'XI-RPL-2', 0);
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
